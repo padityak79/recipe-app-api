56a0305608f57461c67568f714ef92caf6be1128
P Aditya Kumar <padityak79@gmail.com>
2024-03-05 23:50:07 +0530

    tests for admin-side create user page rendering

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -32,7 +32,13 @@ class AdminFunctionalitiesTest(TestCase):
         self.assertContains(response, self.user.name)
 
     def test_admin_edit_user_page(self):
-        """test working of the edit user page"""
+        """test working of the admin-side edit user page"""
         url = reverse('admin:core_user_change', args=[self.user.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_create_user_page(self):
+        """test working of the admin-side create user page"""
+        url = reverse('admin:core_user_add')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
