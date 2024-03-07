25f767f053c0c1500021da2e31ca51051abaf867
padityak79 <padityak79@gmail.com>
2024-03-07 12:55:35 +0530

    Retrieve/Update user API  (#8)
    * Unit tests for retrieve/update user functionalities
    
    * implemented retrieve/update user APIs

--- app/user/urls.py
+++ app/user/urls.py
@@ -3,7 +3,7 @@ Urls for the User Service/App.
 """
 
 from django.urls import path
-from user.views import CreateUserView, AuthTokenView
+from user.views import CreateUserView, AuthTokenView, ManageUserView
 
 
 app_name = 'user'
@@ -11,4 +11,5 @@ app_name = 'user'
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
     path('token/', AuthTokenView.as_view(), name='token'),
+    path('me/', ManageUserView.as_view(), name='me'),
 ]
