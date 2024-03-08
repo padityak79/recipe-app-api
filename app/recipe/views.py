3c5e249a8cbd41e5aaf3822a2260c46650cfb76d
P Aditya Kumar <padityak79@gmail.com>
2024-03-08 18:25:23 +0530

    retrieve detailed recipe functionality added.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,7 +4,7 @@ Views for recipe API.
 
 from core.models import Recipe
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
@@ -13,7 +13,7 @@ from rest_framework.permissions import IsAuthenticated
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """ViewSet for the Recipe Model"""
-    serializer_class = RecipeSerializer
+    serializer_class = RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -21,3 +21,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """retrieve all recipe objects of an authenticated user"""
         return Recipe.objects.filter(user=self.request.user).order_by("-id")
+
+    def get_serializer_class(self):
+        """retrieve serializer_class for the the ViewSet."""
+        if self.action == 'list':
+            return RecipeSerializer
+
+        return self.serializer_class
