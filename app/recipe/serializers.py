8b84ebb5d05ca97769d604823941eec66e1b54c7
P Aditya Kumar <padityak79@gmail.com>
2024-03-08 16:59:52 +0530

    implemented list recipe for authorized users.

---
+++ app/recipe/serializers.py
@@ -0,0 +1,14 @@
+"""
+Serializer for the Recipe Model.
+"""
+
+from core.models import Recipe
+from rest_framework import serializers
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for the Recipe model."""
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -23,7 +23,7 @@ def create_recipe(user, **params):
     """create and return a recipe object."""
     defaults = {
         'title': 'Sample Test Recipe',
-        'time_taken': 10,
+        'time_minutes': 10,
         'description': 'Sample Test Recipe Desciption.',
         'price': Decimal('3.45'),
         'link': 'http://example.com/recipe.pdf',
@@ -57,9 +57,9 @@ class PrivateRecipeAPITests(TestCase):
             'testuser@example.com',
             'testuser123',
         )
-        self.client.force_authorize(self.user)
+        self.client.force_authenticate(self.user)
 
-    def list_retrive_recipes(self):
+    def test_list_retrive_recipes(self):
         "test retrieving a list of recipes."
         create_recipe(self.user)
         create_recipe(self.user)
@@ -70,7 +70,7 @@ class PrivateRecipeAPITests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, recipeSerailizer.data)
 
-    def list_recipe_for_authorized_user(self):
+    def test_list_recipe_for_authorized_user(self):
         """Test list of recipes is limited to authencated user."""
         new_user = get_user_model().objects.create_user(
             'new_user@example.com',
@@ -79,7 +79,7 @@ class PrivateRecipeAPITests(TestCase):
         create_recipe(new_user)
         create_recipe(self.user)
 
-        response = self.client.geet(RECIPES_LIST)
+        response = self.client.get(RECIPES_LIST)
         recipes = Recipe.objects.filter(user=self.user)
         recipeSerializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
