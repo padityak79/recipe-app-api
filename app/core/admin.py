ddcec59fbc8d0881e6f7e81fc9ac6f5d43694984
P Aditya Kumar <padityak79@gmail.com>
2024-03-10 20:41:38 +0530

    implemented ingredient model & test

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,7 +4,12 @@ User Admin Functionalities.
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from core.models import Tag, User, Recipe
+from core.models import(
+    Ingredient,
+    Tag,
+    User,
+    Recipe
+)
 from django.utils.translation import gettext_lazy as _
 
 
@@ -44,3 +49,4 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(User, UserAdmin)
 admin.site.register(Recipe)
 admin.site.register(Tag)
+admin.site.register(Ingredient)
